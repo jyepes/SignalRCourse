4c98677fb8c86c3349f51c5225331e35d796b27f
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-04-29 07:13:41 -0500

    Chat de grupos

---
+++ GroupsChat/Middleware/ChatConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GroupsChat.Middleware
+{
+    public class ChatConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            System.Collections.Specialized.NameValueCollection queryString = HttpContext.Current.Request.QueryString;
+            string nombreGrupo = queryString["groupChat"];
+            return Groups.Add(connectionId, nombreGrupo);
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            string[] decoded = data.Split(':');
+            string groupName = decoded[0];
+            string userName = decoded[1];
+            string message = decoded[2];
+
+            return Groups.Send(groupName, userName + " dice: " + message);
+        }
+    }
+}
