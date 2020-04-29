d7d86e4c1719402089eff7bf8efd2fc5cb0f9df9
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-04-29 14:54:47 -0500

    Broadcast con el modelo de transmisión de datos por Hub

---
+++ Broadcast/Hubs/BroadcastHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Broadcast.Hubs
+{
+    public class BroadcastHub : Hub
+    {
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.displayText(message);
+        }
+    }
+}
---
+++ Broadcast/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Builder;
+using Owin;
+
+[assembly: OwinStartup(typeof(Broadcast.Startup))]
+
+namespace Broadcast
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+
+
+        }
+    }
+}
