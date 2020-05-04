81cf1755022dcf33e82b22d72315d7c1121db7c4
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-05-04 14:16:18 -0500

    Nuevo proyecto: contador de visitas

---
+++ VisitsCounter/Hubs/CounterHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace VisitsCounter.Hubs
+{
+    public class CounterHub : Hub
+    {
+        private static int _visitantes = 0;
+
+        public override async Task OnConnected()
+        {
+            Interlocked.Increment(ref _visitantes);
+            await Clients.Others.Message("Nuevo conexión " + Context.ConnectionId + " Total Visistantes: " + _visitantes);
+            await Clients.Caller.Message("Hola, Bienvenido...!!!");
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Interlocked.Decrement(ref _visitantes);
+            return Clients.All.Message(Context.ConnectionId + " cerro la conexión." + " Total Visistantes: " + _visitantes);
+        }
+
+        public Task Broadcast(string message)
+        {
+            return Clients.All.Message(Context.ConnectionId + ">>" + message);
+        }
+    }
+}
---
+++ VisitsCounter/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(VisitsCounter.Startup))]
+
+namespace VisitsCounter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR("/realtime", new Microsoft.AspNet.SignalR.HubConfiguration());
+        }
+    }
+}
