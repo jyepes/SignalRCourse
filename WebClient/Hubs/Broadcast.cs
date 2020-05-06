bd552aa355b7a1d57674ecd1df1d6bc09a5a63fe
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-05-06 10:19:22 -0500

    Ajustes de código

--- WebClient/Hubs/Broadcast.cs
+++ WebClient/Hubs/Broadcast.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace WebClient.Hubs
 {
